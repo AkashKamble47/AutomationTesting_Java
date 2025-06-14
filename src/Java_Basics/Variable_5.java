b21b326d914cf0ff6649fe98b4a3d9fdeaa4c65b
AkashKamble47 <akashvkamble98@gmail.com>
2025-06-14 15:37:08 +0530

    first commit

---
+++ src/Java_Basics/Variable_5.java
@@ -0,0 +1,14 @@
+package Java_Basics;
+
+public class Variable_5 {
+
+    public static void main(String[] args) {
+
+        int age = 27;
+
+        System.out.println(age);
+
+        System.out.printf("Your age is %d",age);
+
+    }
+}
